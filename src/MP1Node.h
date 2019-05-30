97da5c0a5c6a8f6e3c28b1cc8f74daf419ab76e0
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-05-29 23:50:01 -0400

    update nodeLoopOps

---
+++ src/MP1Node.h
@@ -0,0 +1,77 @@
+/**********************************
+ * FILE NAME: MP1Node.cpp
+ *
+ * DESCRIPTION: Membership protocol run by this Node.
+ * 				Header file of MP1Node class.
+ **********************************/
+
+#ifndef _MP1NODE_H_
+#define _MP1NODE_H_
+
+#include "EmulNet.h"
+#include "Log.h"
+#include "Member.h"
+#include "Params.h"
+#include "Queue.h"
+#include "stdincludes.h"
+
+/**
+ * Macros
+ */
+#define TREMOVE 20
+#define TFAIL 5
+
+/*
+ * Note: You can change/add any functions in MP1Node.{h,cpp}
+ */
+
+/**
+ * Message Types
+ */
+enum MsgTypes { JOINREQ, JOINREP, HEARTBEAT, DUMMYLASTMSGTYPE };
+
+/**
+ * STRUCT NAME: MessageHdr
+ *
+ * DESCRIPTION: Header and content of a message
+ */
+typedef struct MessageHdr {
+  enum MsgTypes msgType;
+} MessageHdr;
+
+/**
+ * CLASS NAME: MP1Node
+ *
+ * DESCRIPTION: Class implementing Membership protocol functionalities for
+ * failure detection
+ */
+class MP1Node {
+ private:
+  EmulNet *emulNet;
+  Log *log;
+  Params *par;
+  Member *memberNode;
+  char NULLADDR[6];
+
+ public:
+  MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
+  Member *getMemberNode() { return memberNode; }
+  int recvLoop();
+  static int enqueueWrapper(void *env, char *buff, int size);
+  void nodeStart(char *servaddrstr, short serverport);
+  int initThisNode(Address *joinaddr);
+  int introduceSelfToGroup(Address *joinAddress);
+  int finishUpThisNode();
+  void nodeLoop();
+  void checkMessages();
+  bool recvCallBack(void *env, char *data, int size);
+  void nodeLoopOps();
+  int isNullAddress(Address *addr);
+  Address getJoinAddress();
+  Address getMemberAddress(int id, int port);
+  void initMemberListTable(Member *memberNode);
+  void printAddress(Address *addr);
+  virtual ~MP1Node();
+};
+
+#endif /* _MP1NODE_H_ */
